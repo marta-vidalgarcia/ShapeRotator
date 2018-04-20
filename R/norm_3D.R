dd531a766e2715dfe66cbc0b0ebe82898544daaf
marta-vidalgarcia <marta.vidalga@gmail.com>
2018-04-20 19:15:05 +1000

    Update norm_3D.R

--- R/norm_3D.R
+++ R/norm_3D.R
@@ -1,4 +1,12 @@
-# Obtain the length of a vector u
+#' norm_3D internal function
+#'
+#' This function allows you to obtain the length of a vector u
+#' @param u vector
+#' @export
+#' @examples
+#' norm_3D()
+
+
 norm_3D <- function (u)
 {
   return( sqrt(u[1]^2 + u[2]^2 + u[3]^2));}
